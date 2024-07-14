2aa45a612749aa10c4ceef27f713712c6588e594
naveen chelloji <chelloji.naveen@gmail.com>
2024-07-14 22:33:43 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,42 +14,82 @@ import { debounceTime } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'Language-Translater';
   suggetions: string[] = [];
-  TranslatedText = '';
-  Languages = [{name: 'Telugu',value: 'te'},{name: 'Hindi',value: 'hi',},{name: 'Tamil',value: 'ta',},{name: 'Kannada',value: 'kn',}, ];
+  finalTranslatedText = '';
+
+  Languages = [
+    { name: 'Telugu', value: 'te' },
+    { name: 'Hindi', value: 'hi' },
+    { name: 'Tamil', value: 'ta' },
+    { name: 'Kannada', value: 'kn' },
+  ];
   Form = new FormGroup({
     inputText: new FormControl(''),
     translatedText: new FormControl(''),
-    language: new FormControl(' '),
-    suggest: new FormControl(' '),
+    language: new FormControl(''),
+    suggest: new FormControl(''),
   });
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.Form.get('inputText')
-      ?.valueChanges.pipe(debounceTime(300))
+      ?.valueChanges.pipe(debounceTime(100))
       .subscribe((value) => {
-        this.getTranslatedText(value);
+        if (value?.length !== 0) {
+          this.getTranslatedText(value).subscribe((data: any) => {
+            this.Form.patchValue({ translatedText: data[1][0][1][0] });
+          });
+        } else {
+          this.Form.patchValue({ translatedText: '' });
+        }
       });
+
+    this.Form.get('translatedText')?.valueChanges.subscribe((value: any) => {
+      if (this.Form.get('translatedText')?.dirty === true) {
+        let alphaString = '';
+        for (let i of value?.split(' ')) {
+          if (/[a-zA-Z]/.test(i)) {
+            alphaString = i;
+            break;
+          }
+        }
+        if (alphaString.length !== 0) {
+          this.getTranslatedText(alphaString).subscribe((data: any) => {
+            this.suggetions = data[1][0][1];
+          });
+        }
+      }
+    });
+
     this.Form.get('suggest')?.valueChanges.subscribe((value: any) => {
-      this.Form.patchValue({
-        translatedText: this.Form.get('translatedText')?.value + ' ' + value,
-      });
-      this.Form.patchValue({ inputText: '' });
-      this.suggetions = [];
+      if (this.Form.get('suggest')?.value?.length !== 0) {
+        const alphaText: any =this.Form.get('translatedText')?.value?.split(' ');
+        let convertedText = '';
+        for (let i in alphaText) {
+          if (/[a-zA-Z]/.test(alphaText[i])) {
+            convertedText = convertedText + value + ' ';
+          } else {
+            convertedText = convertedText + alphaText[i] + ' ';
+          }
+        }
+        this.Form.get('translatedText')?.reset();
+        this.Form.patchValue({
+          translatedText: convertedText,
+        });
+        this.suggetions = [];
+        this.Form.get('suggest')?.patchValue('');
+      }
     });
   }
-  getTranslatedText(value: string | null) {
-    if (value?.trim().length !== 0) {
-      this.http
-        .get(
-          `https://inputtools.google.com/request?text=${value}&itc=${this.Form.value.language}-t-i0-und&num=13&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage`
-        )
-        .subscribe((data: any) => {
-          this.suggetions = data[1][0][1];
-        });
-    }
+
+  getTranslatedText(value: string | null): any {
+    return this.http.get(
+      `https://inputtools.google.com/request?text=${value}&itc=${
+        this.Form.value.language === '' ? 'te' : this.Form.value.language
+      }-t-i0-und&num=13&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage`
+    );
   }
+  
   FinalText() {
-    this.TranslatedText = this.Form.value.translatedText || '';
-    this.Form.patchValue({ translatedText: '' });
+    this.finalTranslatedText = this.Form.value.translatedText || '';
+    this.Form.patchValue({ translatedText: '', inputText: '' });
   }
 }
